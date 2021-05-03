3bc7593078f62f21da948a1dec53215b0213ce82
ShwethaRavitej <74089416+ShwethaRavitej@users.noreply.github.com>
2021-05-03 02:56:15 -0700

    first change

--- src/App.js
+++ src/App.js
@@ -3,8 +3,8 @@ import "./styles.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <h1>Hello CodingHero</h1>
+      <h2>MY FIRST APP</h2>
     </div>
   );
 }
